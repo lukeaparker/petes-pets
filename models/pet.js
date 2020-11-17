676e23f6f4b7bab03e5d7a9dbb5418837b161abc
Luke Parker <lukeparker@Lukes-MacBook-Pro.local>
2020-11-16 18:04:40 -0800

    added pagination

--- models/pet.js
+++ models/pet.js
@@ -3,17 +3,27 @@
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
-const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
-});
+
+        const mongoosePaginate = require('mongoose-paginate');
+
+        mongoosePaginate.paginate.options = {
+          limit: 6 // how many records on each page
+        };
+
+        const PetSchema = new Schema({
+            name            : { type: String, required: true }
+          , species         : { type: String }
+          , birthday        : { type: Date }
+          , picUrl          : { type: String }
+          , picUrlSq        : { type: String }
+          , favoriteFood    : { type: String }
+          , description     : { type: String }
+        },
+        {
+          timestamps: true
+        });
+
+        PetSchema.plugin(mongoosePaginate);
+
 
 module.exports = mongoose.model('Pet', PetSchema);
